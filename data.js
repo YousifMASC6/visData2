01938de37f713bb1d88358526cd1e1585b6c3b83
YousifMASC6git config --global user.name YousifMASC6git config --global user.name YousifMASC6 <Yousifmoftah@gmail.com>
2019-08-05 14:32:10 -0400

    Not including filter

--- data.js
+++ data.js
@@ -6,6 +6,37 @@ fetch(url).then(function(response){
 
 .then(function(myJson){
     const json = myJson.results;
-    console.log(json);
-    
+    // console.log(json[0]);
+    const main = document.querySelector("main")
+    console.log(main)
+
+const createCard = (obj, indx) => {
+    const newCard = document.createElement("div")
+    newCard.className = "card"
+    newCard.id = indx;
+
+    const face = document.createElement("img")
+    face.src = obj.picture.medium
+    face.alt = "Portrait"
+
+    const newText = document.createElement("p")
+    newText.innerText = `${obj.name.first} ${obj.name.last}
+    ${obj.gender}`
+
+    newCard.appendChild(face)
+    newCard.appendChild(newText)
+    main.appendChild(newCard)
+}
+
+
+
+const loopCreate = arr => {
+    for(let i = 0; i < arr.length; i++){
+        createCard(arr[i], i)
+}
+}
+loopCreate(json)
+
+
 });
+
--- main.js
+++ main.js
@@ -1,21 +1,36 @@
-const main = document.querySelector("main")
-
-const createCard = (obj, indx) => {
-    const newCard = document.createElement("div")
-    newCard.className = "card"
-    newCard.id = indx;
+// const main = document.querySelector("main")
 
 
-const newText = document.createElement("p")
-    newText.innerText = `${obj.name.first} ${obj.name.last}`
-    
-    newCard.appendChild(newText)}
+// const createCard = (obj, indx) => {
+//     const newCard = document.createElement("div")
+//     newCard.className = "card"
+//     newCard.id = indx;
 
-    const loopCreate = arr => {
-        for(let i = 0; i < arr.length; i++){
-            createCard(arr[i], i)
-    }
-    loopCreate(json)
-    }
-    
-      
+//     const face = document.createElement("img")
+//     face.src = obj.picture.medium
+//     face.alt = "Portrait"
+
+//     const newText = document.createElement("p")
+//     newText.innerText = `${obj.name.first} ${obj.name.last}
+//     ${obj.location.city} ${obj.location.state}
+//     ${obj.email}
+//     ${obj.login.username}
+//     ${obj.dob.age}`
+
+
+//     newCard.appendChild(face)
+//     newCard.appendChild(newText)
+//     main.appendChild(newCard)
+// }
+
+
+
+// const loopCreate = arr => {
+//     for(let i = 0; i < arr.length; i++){
+//         createCard(arr[i], i)
+// }
+// }
+
+// loopCreate(json)
+
+
